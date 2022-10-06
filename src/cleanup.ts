79c83fbc0d224574602363bb9976ef3e19644859
Plagiatus <lukas.scheuerle@web.de>
2022-10-06 12:36:55 +0300

    more cleanup files

--- src/cleanup.ts
+++ src/cleanup.ts
@@ -7,6 +7,23 @@ import { client, db } from "./main";
 
 async function cleanup() {
     await db.connect();
+
+    let guilds = client.guilds;
+    let subscriptions = await db.getSubscriptions();
+
+    await guilds.fetch();
+    for (let s of subscriptions) {
+        for (let g of s.guilds) {
+            if (!guilds.cache.has(g.guild)) {
+                console.log("Found unused guild subscription:", g.guild);
+                let subscriptions = await db.getSubscriptionsInGuild(g.guild);
+                for (let sub of subscriptions) {
+                    await db.removeSubscription(g.guild, sub.subreddit);
+                }
+            }
+        }
+    }
+
     let collections = await db.getAllCollections();
     let collectionNames: string[] = [];
     for (let c of collections) {
@@ -16,21 +33,18 @@ async function cleanup() {
     }
 
     
-    let guilds = client.guilds;
-    let subscriptions = await db.getSubscriptions();
-
-    await guilds.fetch();
-    for(let s of subscriptions){
-        guilds.fetch()
+    subscriptions = await db.getSubscriptions();
+    for (let s of subscriptions) {
         let i = collectionNames.indexOf(s.subreddit);
-        if(i >= 0){
+        if (i >= 0) {
             collectionNames.splice(i, 1);
         }
     }
 
+
     console.log(`There are ${collectionNames.length} unnecessary collections. Removing...`);
-    
-    for(let c of collectionNames){
+
+    for (let c of collectionNames) {
         await db.removeCollection(c);
     }
     console.log(`Removed.`);
